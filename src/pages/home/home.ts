b3f23134ebeafce8326983a135b95b62f177a6af
unknown <anselmedu25@hotmail.com>
2019-01-21 10:40:40 +0100

    Add login-page info messages when wrong password etc.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -19,20 +19,22 @@ import {
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 	
 	login_form : FormGroup;
 
   constructor(public navCtrl: NavController, private formBuilder : FormBuilder, private http : HttpClient) {
-	  
+		this.conn_status = '';
+		
         this.login_form = this.formBuilder.group({
 
             password: new FormControl('', Validators.compose([
-                Validators.required,
+                Validators.required
             ])),
 
             mail: new FormControl('', Validators.compose([
-                Validators.required,
+                Validators.required
             ]))
 		});
 
@@ -44,6 +46,12 @@ export class HomePage {
   
   register() {
 	  this.navCtrl.push(RegisterPage);
+  }
+  
+  connected() {
+	  if(this.conn_status=='unknown_user')
+		  return true;
+	  else return false;
   }
   
   connect() {
@@ -66,14 +74,28 @@ export class HomePage {
 
 				result = result['message'];
 				
+				this.conn_status = result;
+				
                 if(result == 'connected') {
 					this.navCtrl.push(JoinPage);
 				}
 				else if(result == 'no_such_user') {
 					console.log('Unknown user.');
+					this.login_form.controls['mail'].setErrors({
+                "unknown_mail": true
+					});
+					this.login_form.controls['password'].setErrors({
+                "wrong_password": false
+					});
 				}
 				else if(result == 'wrong_password') {
 					console.log('Wrong password');
+					this.login_form.controls['password'].setErrors({
+                "wrong_password": true
+            });
+			this.login_form.controls['mail'].setErrors({
+                "unknown_mail": false
+					});
 				}
 				else {
 					console.log('error');
